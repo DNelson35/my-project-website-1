c5a12e042098b998a914a86cddea85e27bcc22ff
Damien Nelson <dhnisaman@gmail.com>
2022-10-29 16:35:34 -0400

    added log, and form. liked soicial media

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
 /*const h2 = document.createElement("h2");
 h2.textContent = "this content added by JavaScript";
 
